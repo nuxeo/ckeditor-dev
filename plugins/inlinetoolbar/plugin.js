932cbc451b43c00038e39b7c91eee16aeb2e5244
Wojciech Wozniak <wwozniak92@gmail.com>
2017-10-10 02:44:17 +0200

    Remove button option and unit tests. And mismerge fix.

--- plugins/inlinetoolbar/plugin.js
+++ plugins/inlinetoolbar/plugin.js
@@ -98,7 +98,7 @@
 			/**
 			 * Places the inline toolbar next to a specified element so the tip of the toolbar's triangle
 			 * touches that element.
-			 *
+4			 *
 			 * @method create
 			 * @param {CKEDITOR.dom.element} element The element to which the panel is attached.
 			 */
@@ -127,23 +127,21 @@
 			};
 
 			/**
-			 * Adds an item from the specified definition to the editor context menu.
+			 * Adds an item from the specified definition to the inline toolbar.
 			 *
 			 * @method
 			 * @param {String} name The menu item name.
 			 * @param {Object} definition The menu item definition.
-			 * @member CKEDITOR.editor
 			 */
 			CKEDITOR.ui.inlineToolbarView.prototype.addMenuItem = function( name, definition ) {
 				this.menuItems[ name ] = new CKEDITOR.ui.button( definition );
 			};
 
 			/**
-			 * Adds one or more items from the specified definition object to the editor context menu.
+			 * Adds one or more items from the specified definition object to the inline toolbar.
 			 *
 			 * @method
 			 * @param {Object} definitions Object where keys are used as itemName and corresponding values as definition for a {@link #addMenuItem} call.
-			 * @member CKEDITOR.editor
 			 */
 			CKEDITOR.ui.inlineToolbarView.prototype.addMenuItems = function( definitions ) {
 				for ( var itemName in definitions ) {
@@ -152,15 +150,27 @@
 			};
 
 			/**
-			 * Retrieves a particular menu item definition from the editor context menu.
+			 * Retrieves a particular menu item definition from the inline toolbar.
 			 *
 			 * @method
 			 * @param {String} name The name of the desired menu item.
 			 * @returns {Object}
-			 * @member CKEDITOR.editor
 			 */
 			CKEDITOR.ui.inlineToolbarView.prototype.getMenuItem = function( name ) {
 				return this.menuItems[ name ];
+			};
+
+			/**
+			 * Removes a particular menu item definition from the inline toolbar.
+			 *
+			 * @method
+			 * @param {String} name The name of the desired menu item.
+			 * @returns {Object}
+			 */
+			CKEDITOR.ui.inlineToolbarView.prototype.deleteMenuItem = function( name ) {
+				if ( this.menuItems[ name ] ) {
+					delete this.menuItems[ name ];
+				}
 			};
 		}
 	} );
